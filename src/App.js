f3f17dcc1be93f37d7cde4ab927b8ae950db5542
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-04 04:35:33 +0600

    blogs component created

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Blogs from "./Components/Blogs/Blogs";
 import DashBoard from "./Components/DashBoard/DashBoard";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='reviews/' element={<Reviews></Reviews>}></Route>
         <Route path='dashboard/' element={<DashBoard></DashBoard>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
