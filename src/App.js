713e1e47c6ff226c060f0fcab80c0632f012cad2
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-04-04 07:09:34 +0600

    assignment full complated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import About from "./Components/About/About";
 import Blogs from "./Components/Blogs/Blogs";
 import DashBoard from "./Components/DashBoard/DashBoard";
 import Header from "./Components/Header/Header";
@@ -15,7 +16,8 @@ function App() {
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='reviews/' element={<Reviews></Reviews>}></Route>
         <Route path='dashboard/' element={<DashBoard></DashBoard>}></Route>
-        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='blogs/' element={<Blogs></Blogs>}></Route>
+        <Route path='about/' element={<About></About>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
 
       </Routes>
